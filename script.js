ecb09477d435d9f373e02d6f9367f9d987c9e122
AlbertTumo1 <levon.davtyan1@tumo.org>
2023-05-17 17:29:50 +0400

    test, lesson 5

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const enemyEaterArr = [];
 // Git commit -m "descripion"
 // git push -u origin master
 
-// Programming 3, Lesson 2 Github testing
+// Programming 3, Lesson 5
 
 const matrix = [];
 const a = 16;
