0225ff1f2b3488f93a69bdb67b322a5c7cedc421
SergeyEngibaryan <sergey.y@tumo.org>
2023-05-24 17:42:25 +0400

    lesson 7, matrix generated on backend

--- script.js
+++ script.js
@@ -2,53 +2,27 @@
 // Git commit -m "descripion"
 // git push -u origin master
 
-const side = 50;
-const grassArr = [];
-const grassEaterArr = [];
-const predatorArr = [];
-const kingEaterArr = [];
-const enemyEaterArr = [];
+const side = 30;
+const m = 16;
+const n = 16;
 
-let myMatrix = [];
 let socket = io();
 
-const matrix = [];
-const a = 16;
-const b = 16;
-
-function GeneratePlayers(count, character){
-    let p = 0;
-    while (p < count) {
-        let k = Math.floor(random(0,a))
-        let l = Math.floor(random(0,b))
-        if(matrix[k][l] == 0){
-            matrix[k][l] = character
-        }
-        p++;
-    }
-}
-
 function setup() {
-    createCanvas(myMatrix[0].length * side, myMatrix.length * side);
+    frameRate(1);
+    createCanvas(m * side, n * side);
     background('#acacac');
-
-    GeneratePlayers(80,1); // grass
-    GeneratePlayers(12,2); // grass eater
-    GeneratePlayers(14,3); // predator
-    GeneratePlayers(2,4); // king eater
-    GeneratePlayers(9,5); // enemy eater (eats only grassEater and Predator)
-
-    
 }
 
-function drawing(matrix) {
+function drawing(data) {
+    let matrix = data.matrix;
     for (let y = 0; y < matrix.length; y++) {
         for (let x = 0; x < matrix[y].length; x++) {
             if (matrix[y][x] == 0) {
                 fill("#acacac");
             } else if (matrix[y][x] == 1) {
                 fill("green");
-            } else if (matrix[y][x]==2){
+            } else if (matrix[y][x] == 2){
                 fill("yellow")
             } else if (matrix[y][x] == 3) {
                 fill("red");
@@ -63,5 +37,4 @@ function drawing(matrix) {
     }
 }
 
-socket.on("initial", (data) => myMatrix = data);
-socket.on("send_matrix", (data) => drawing(data));
+socket.on("matrix", drawing);
--- server.js
+++ server.js
@@ -20,10 +20,68 @@ predatorArr = [];
 kingEaterArr = [];
 enemyEaterArr = [];
 
-const matrix = [];
+matrix = [];
 
 const a = 16;
 const b = 16;
+
+function GeneratePlayers(count, character){
+    let p = 0;
+    while (p < count) {
+        let k = Math.floor(Math.random() * a)
+        let l = Math.floor(Math.random() * b)
+        if(matrix[k][l] == 0){
+            matrix[k][l] = character
+        }
+        p++;
+    }
+}
+
+function GenerateMatrix() {
+    for (let i = 0; i < a; i++) {
+        matrix.push([]);
+
+        for (let j = 0; j < b; j++) {
+             matrix[i].push(0)
+        }
+    }
+
+    GeneratePlayers(80,1); // grass
+    GeneratePlayers(20,2); // grass eater
+    GeneratePlayers(17,3); // predator
+    GeneratePlayers(6,4); // king eater
+    GeneratePlayers(12,5); // enemy eater (eats only grassEater and Predator)
+    return matrix;
+}
+
+matrix = GenerateMatrix();
+console.log(matrix);
+    
+for(let y = 0; y < matrix.length; ++y){
+    console.log(matrix)
+    for(let x = 0; x < matrix[y].length; ++x){
+        if(matrix[y][x] == 1){
+            let grass = new Grass(x,y,1);
+            grassArr.push(grass);
+        }
+        else if(matrix[y][x] == 2){
+            let grassEater = new GrassEater(x,y,2)
+            grassEaterArr.push(grassEater)
+        } 
+        else if(matrix[y][x] == 3){
+            let predator = new Predator(x,y,3)
+            predatorArr.push(predator);
+        }  
+        else if(matrix[y][x] == 4){
+            let kingEater = new KingEater(x,y,4);
+            kingEaterArr.push(kingEater);
+        }  
+        else if(matrix[y][x] == 5){
+            let enemyEater = new EnemyEater(x,y,5);
+            enemyEaterArr.push(enemyEater);
+        }  
+    }
+}
 
 function drawGame() {
     for(let i in grassArr){
@@ -46,63 +104,15 @@ function drawGame() {
         enemyEaterArr[i].eat();   
     }
 
-    console.log(matrix)
-    return matrix;
-}
-
-function createCanvas() {
-    function GenerateMatrix() {
-        for (let i = 0; i < a; i++) {
-            matrix.push([])
-    
-            for (let j = 0; j < b; j++) {
-                matrix[i].push(0)
-            }
-        }
-
-        return matrix;
+    let sendData = {
+      matrix: matrix
     }
 
-    GenerateMatrix();
-    console.log(matrix)
-
-
-    for(let y = 0; y < matrix.length; ++y){
-        for(let x = 0; x < matrix[y].length; ++x){
-            if(matrix[y][x] == 1){
-                let grass = new Grass(x,y,1);
-                grassArr.push(grass);
-            }
-            else if(matrix[y][x] == 2){
-                let grassEater = new GrassEater(x,y,2)
-                grassEaterArr.push(grassEater)
-            } 
-            else if(matrix[y][x] == 3){
-                let predator = new Predator(x,y,3)
-                predatorArr.push(predator);
-            }  
-            else if(matrix[y][x] == 4){
-                let kingEater = new KingEater(x,y,4);
-                kingEaterArr.push(kingEater);
-            }  
-            else if(matrix[y][x] == 5){
-                let enemyEater = new EnemyEater(x,y,5);
-                enemyEaterArr.push(enemyEater);
-            }  
-            
-        }
-     }
+    console.log(sendData);
+  
+    io.sockets.emit("matrix", sendData)
 }
 
-createCanvas();
-
-setInterval(() => {
-    drawGame();
-}, 1000);
-
-io.on('connection', function(socket) {
-    socket.emit("initial", matrix);
-    socket.emit("send_matrix", matrix);
-});
+setInterval(drawGame, 1000);
 
 server.listen(3000, () => console.log("Server running on port 3000! COOL"));
